51a78f53f692d60e6b719fced6fd01de7a53f826
Mahmoud Elmougy <mahmoud.elmougy1@gmail.com>
2023-11-10 10:47:56 +0200

    Login update

--- src/components/ProtectedRoute/ProtectedRoute.jsx
+++ src/components/ProtectedRoute/ProtectedRoute.jsx
@@ -1,9 +1,8 @@
 import React, { useContext } from 'react'
 import { Navigate } from 'react-router-dom'
-import { UserContext } from '../TokenContext/TokenContext'
 
 export default function ProtectedRoute(props) {
-  let {userToken} = useContext(UserContext)
+  
   if(localStorage.getItem('usertoken') !== null){
     return props.children
   }else{
