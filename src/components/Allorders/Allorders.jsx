d7b0a8da634ffa382b58d50663069a2d661f9160
Mahmoud Elmougy <mahmoud.elmougy1@gmail.com>
2023-11-22 13:53:06 +0200

    update allorders ui

--- src/components/Allorders/Allorders.jsx
+++ src/components/Allorders/Allorders.jsx
@@ -45,14 +45,14 @@ export default function Allorders() {
           <h1 className='text-center text-main my-4 '>Your Orders</h1>
           <div className="row justify-content-between g-3  ">
             {userOrders.map((order , idx)=>{
-             return <div key={idx} className="col-md-5 bg-main-light rounded-4 p-3 ">
-                <span className='h6 me-3 text-main'>total cost: {order.totalOrderPrice} EGP</span>
-                <span className='h6  text-main'>payment method: {order.paymentMethodType}</span>
+             return <div key={idx} className="col-12 bg-main-light rounded-4 p-3 ">
+                <span className='h4 me-3 text-main'>Total Cost: {order.totalOrderPrice} EGP</span>
+                <span className='h4  text-main'>Payment Method: {order.paymentMethodType}</span>
                 <p className='m-0 mt-3'>phone: {order.shippingAddress.phone}</p>
                 <p className='m-0 '>city: {order.shippingAddress.city}</p>
                 <div className='row'>
                   {order.cartItems.map((item , index)=>{
-                    return <div key={index} className=' col-md-5 bg-main text-white rounded-3 p-3 m-2 '>
+                    return <div key={index} className='col-sm-5 col-md-3 col-xl-2 bg-main text-white rounded-3 p-3 m-2 '>
                       <p className=''>product count : {item.count}</p>
                       <p className=''> price : {item.price} EGP</p>
                     </div>
